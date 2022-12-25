12302ee24c8bf534b4e0d1463e8b3c24ce8eded5
Austin Schonfeld <austinschonfeld@gmail.com>
2022-12-24 20:12:31 -0700

    new branch

---
+++ book_scraper/scraper.py
@@ -0,0 +1,19 @@
+#this comment is to test initial GitHub commit
+from bs4 import BeautifulSoup as bs
+from urllib.request import urlopen
+import pandas as pd
+
+#save the website url
+url = 'http://books.toscrape.com/catalogue/page-1.html'
+#open the url
+page = urlopen(url)
+#read the website html
+html = page.read().decode('utf-8')
+#create a soup object
+soup = bs(html, 'html.parser')
+#grab the product section of each book entry
+section = soup.find(class_ = 'product_pod').find('a').find('img')
+print(section.attrs['alt'])
+
+
+
